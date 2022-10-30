daab307648480f0535042328da41e0cb222f64bc
wdeath <wladimir134679@gmail.com>
2022-10-30 16:59:41 +0300

    fix charts

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,23 +1,29 @@
 <template>
 
   <v-container>
-    <v-card>
-      <v-card-title>
-        Главная страница
-      </v-card-title>
-      <v-card-text>
-        Информация о проекте можно посмотреть
-        <router-link to="/about-the-project">тут</router-link>
-      </v-card-text>
-    </v-card>
-    <v-card>
-      <v-card-title>
-        Test charts
-      </v-card-title>
-      <v-card-actions>
-        <apexchart :height="200" :width="500" :series="series" :options="options"/>
-      </v-card-actions>
-    </v-card>
+    <v-row>
+      <v-col cols="12">
+        <v-card>
+          <v-card-title>
+            Главная страница
+          </v-card-title>
+          <v-card-text>
+            Информация о проекте можно посмотреть
+            <router-link to="/about-the-project">тут</router-link>
+          </v-card-text>
+        </v-card>
+      </v-col>
+      <v-col cols="12">
+        <v-card>
+          <v-card-title>
+            Test charts
+          </v-card-title>
+          <v-card-text>
+            <apexchart class="pa-5" width="100%" height="300" :series="series" :options="options"/>
+          </v-card-text>
+        </v-card>
+      </v-col>
+    </v-row>
   </v-container>
 </template>
 
@@ -37,11 +43,19 @@ export default {
           size: 4,
         },
         xaxis: {
-          type: "datetime"
+          type: "datetime",
+          title: {
+            text: "Время"
+          }
+        },
+        yaxis: {
+          title: {
+            text: "Пользователи"
+          }
         }
       },
       series: [{
-        name: 'series-1',
+        name: 'Подписка',
         data: [
           {
             x: new Date(2022, 10, 10).getTime(),
@@ -58,6 +72,18 @@ export default {
           {
             x: new Date(2022, 10, 20).getTime(),
             y: 15
+          },
+          {
+            x: new Date(2022, 10, 21).getTime(),
+            y: 10
+          },
+          {
+            x: new Date(2022, 10, 22).getTime(),
+            y: 20
+          },
+          {
+            x: new Date(2022, 10, 23).getTime(),
+            y: 14
           },
         ]
       }]
