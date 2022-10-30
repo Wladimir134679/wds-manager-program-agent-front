fe25ee0644c50a8f92cc8e546725240edb82b773
wdeath <wladimir134679@gmail.com>
2022-10-30 16:26:22 +0300

    new vue js lib for charts timeline

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -6,15 +6,66 @@
         Главная страница
       </v-card-title>
       <v-card-text>
-        Информация о проекте можно посмотреть <router-link to="/about-the-project">тут</router-link>
+        Информация о проекте можно посмотреть
+        <router-link to="/about-the-project">тут</router-link>
       </v-card-text>
+    </v-card>
+    <v-card>
+      <v-card-title>
+        Test charts
+      </v-card-title>
+      <v-card-actions>
+        <apexchart :height="200" :width="500" :series="series" :options="options"/>
+      </v-card-actions>
     </v-card>
   </v-container>
 </template>
 
 <script>
 export default {
-  name: "HomeView"
+  name: "HomeView",
+  data() {
+    return {
+      options: {
+        stroke: {
+          curve: 'straight',
+        },
+        chart: {
+          id: 'vuechart-example',
+        },
+        markers: {
+          size: 4,
+        },
+        xaxis: {
+          type: "datetime"
+        }
+      },
+      series: [{
+        name: 'series-1',
+        data: [
+          {
+            x: new Date(2022, 10, 10).getTime(),
+            y: 10
+          },
+          {
+            x: new Date(2022, 10, 12).getTime(),
+            y: 11
+          },
+          {
+            x: new Date(2022, 10, 13).getTime(),
+            y: 12
+          },
+          {
+            x: new Date(2022, 10, 20).getTime(),
+            y: 15
+          },
+        ]
+      }]
+    }
+  },
+  created() {
+
+  }
 }
 </script>
 
